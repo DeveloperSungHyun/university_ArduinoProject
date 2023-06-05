eb8f37fcb28df9afefa6cdc2d53b765c6b94d5d0
DeverlopSungHyun <hanstudio5322@gmail.com>
2023-06-06 00:12:50 +0900

    segment4ch_display

--- ArduinoIOT_project/ArduinoIOT_project.ino
+++ ArduinoIOT_project/ArduinoIOT_project.ino
@@ -9,8 +9,10 @@ byte data;
 
 int sgmt_ch[4] = {0B00000001, 0B00000010, 0B00000100, 0B00001000};//7세그먼트 자리수
 int sgmt_num[10] = {0B00000011, 0B10011111, 0B00100101, 0B00001101, 0B10011001,
-                   0B01001001, 0B01000001, 0B00011011, 0B00000001, 0B00001001
-};
+                    0B01001001, 0B01000001, 0B00011011, 0B00000001, 0B00001001
+                   };
+
+int ch_num = 0;
 
 void setup() {
   //Start Serial for debuging purposes
@@ -22,17 +24,22 @@ void setup() {
 
 }
 
-int test[4] = {1, 0, 2, 4};
 void loop() {
 
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(latchPin, 0);
+  segment_display(1, 8, 2, 4);
 
-    shiftOut(dataPin, clockPin, LSBFIRST, sgmt_num[test[i]]); // 숫자
-    shiftOut(dataPin, clockPin, MSBFIRST, sgmt_ch[i]); // 자리수
+}
 
-    digitalWrite(latchPin, 1);
-    //delay(1);
-  }
+void segment_display(int num1, int num2, int num3, int num4) {
+  ch_num++;
+  int test[4] = {num1, num2, num3, num4};
 
+  if (ch_num >= 4) ch_num = 0;
+
+  digitalWrite(latchPin, false);
+
+  shiftOut(dataPin, clockPin, LSBFIRST, sgmt_num[test[ch_num]]); // 숫자
+  shiftOut(dataPin, clockPin, MSBFIRST, sgmt_ch[ch_num]); // 자리수
+
+  digitalWrite(latchPin, true);
 }
