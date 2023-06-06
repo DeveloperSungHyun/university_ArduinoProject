b55c795d48f40b374ab4f12952fdbc3cdfd2addf
DeverlopSungHyun <hanstudio5322@gmail.com>
2023-06-06 20:46:07 +0900

    bluetooth TimeSetting

--- ArduinoIOT_project/ArduinoIOT_project.ino
+++ ArduinoIOT_project/ArduinoIOT_project.ino
@@ -1,12 +1,19 @@
 #include <virtuabotixRTC.h>
+#include <SoftwareSerial.h>
+#include <string.h>
 
 virtuabotixRTC myRTC(5, 6, 7);
 
-int latchPin = 3;
-int clockPin = 2;
-int dataPin = 4;
+#define BT_RX 8
+#define BT_TX 9
 
-int lm35Pin = A0;
+#define latchPin 3
+#define clockPin 2
+#define dataPin 4
+
+#define lm35Pin A0
+
+SoftwareSerial bluetooth(BT_RX, BT_TX);
 
 int sgmt_ch[4] = {0B00000001, 0B00000010, 0B00000100, 0B00001000};//7세그먼트 자리수
 int sgmt_num[11] = {0B00000011, 0B10011111, 0B00100101, 0B00001101, 0B10011001,
@@ -31,13 +38,22 @@ long change_time = 0;
 
 bool seconds = true;
 
+String BT_getData = "";
+
+
+
 void setup() {
 
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
 
-  myRTC.setDS1302Time(00, 00, 10, 6, 6, 7, 2023);
+  pinMode(13, OUTPUT);
+
+  myRTC.setDS1302Time(00, 23, 10, 6, 6, 7, 2023);
+
+  bluetooth.begin(9600);
+  Serial.begin(9600);
 
   time_UpDataTime = temp_UpDataTime = millis();
   change_time = millis();
@@ -46,20 +62,22 @@ void setup() {
 float test = 0;
 void loop() {
 
+  bluetooth_getData();
+
   if (millis() - change_time >= change_delay) {
     change_time = millis();
     TimeToTemp_change = !TimeToTemp_change;
   }
 
-  
+
   if (TimeToTemp_change) {//=================시간
-    
-    if(millis() - time_UpDataTime >= 500){
+
+    if (millis() - time_UpDataTime >= 500) {
       time_UpDataTime = millis();
       myRTC.updateTime();//시갑 업데이트
       seconds = !seconds;
     }
-   
+
     TimeDisplay(myRTC.hours, myRTC.minutes, seconds);//시간 출력
 
   } else {//=================================온도
@@ -83,9 +101,39 @@ void loop() {
   }
 
 }
+
+void bluetooth_getData() {
+
+  char c = bluetooth.read();
+  if (bluetooth.available()) {
+    digitalWrite(13, !digitalRead(13));
+    BT_getData += c;
+
+  } else {
+
+    if (BT_getData != "") {
+      Serial.println(BT_getData);
+      if (BT_getData.substring(0, 1) == "t") {
+        myRTC.setDS1302Time(
+          BT_getData.substring(1, 3).toInt(),//초
+          BT_getData.substring(3, 5).toInt(),//분
+          BT_getData.substring(5, 7).toInt(),//시
+          BT_getData.substring(7, 8).toInt(),//요일
+          BT_getData.substring(8, 10).toInt(),//일
+          BT_getData.substring(10, 12).toInt(),//월
+          BT_getData.substring(12, 16).toInt());//년
+      }
+    }
+
+    BT_getData = "";
+  }
+
+}
+
 void TimeDisplay(int h, int m, bool seconds) {
   segment_display(h / 10, h % 10, m / 10, m % 10, -1, seconds, false);
 }
+
 void TemperatureDisplay(float num) {
   segment_display((int)num / 10, (int)num % 10, (int)(num * 10) % 10, 10, 1, false, true);
 }
@@ -101,10 +149,10 @@ void segment_display(int num1, int num2, int num3, int num4, int dot, bool secon
   if (dot == ch_num) dot_value = 1;
   else dot_value = 0;
 
-  if(seconds) seconds_dot = 0B00010000;
+  if (seconds) seconds_dot = 0B00010000;
   else seconds_dot = 0;
 
-  if(tamp) tamp_dot = 0B00100000;
+  if (tamp) tamp_dot = 0B00100000;
   else tamp_dot = 0;
 
   digitalWrite(latchPin, false);
