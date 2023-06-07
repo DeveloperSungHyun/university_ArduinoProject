7c0f1e5142da6cd29c99a7209f71bb6702782032
DeverlopSungHyun <hanstudio5322@gmail.com>
2023-06-07 15:05:57 +0900

    완성

--- ArduinoIOT_project/ArduinoIOT_project.ino
+++ ArduinoIOT_project/ArduinoIOT_project.ino
@@ -1,19 +1,20 @@
 #include <virtuabotixRTC.h>
-#include <SoftwareSerial.h>
 #include <string.h>
 
 virtuabotixRTC myRTC(5, 6, 7);
-
-#define BT_RX 8
-#define BT_TX 9
 
 #define latchPin 3
 #define clockPin 2
 #define dataPin 4
 
+#define Button_alarmsetting 10 //알람 설정버튼
+#define Button_setting 11 //시간 설정버튼
+#define Button_Up 12 //설정버튼
+#define Button_Down 13 //설정버튼
+
 #define lm35Pin A0
 
-SoftwareSerial bluetooth(BT_RX, BT_TX);
+#define SP 10
 
 int sgmt_ch[4] = {0B00000001, 0B00000010, 0B00000100, 0B00001000};//7세그먼트 자리수
 int sgmt_num[11] = {0B00000011, 0B10011111, 0B00100101, 0B00001101, 0B10011001,
@@ -30,7 +31,11 @@ float temperatureC;
 
 long temp_UpDataTime;
 long time_UpDataTime;
+long distance_UpDataTIme;
 
+long buttton_CheckTime;
+
+long alarm_delay;
 
 bool TimeToTemp_change = false;
 int change_delay = 5000;
@@ -38,7 +43,7 @@ long change_time = 0;
 
 bool seconds = true;
 
-String BT_getData = "";
+int alarm_h = 15, alarm_m = 30;
 
 
 
@@ -50,19 +55,57 @@ void setup() {
 
   pinMode(13, OUTPUT);
 
+  pinMode(Button_alarmsetting, INPUT);
+  pinMode(Button_setting, INPUT);
+  pinMode(Button_Up, INPUT);
+  pinMode(Button_Down, INPUT);
+
   myRTC.setDS1302Time(00, 23, 10, 6, 6, 7, 2023);
 
-  bluetooth.begin(9600);
   Serial.begin(9600);
 
-  time_UpDataTime = temp_UpDataTime = millis();
+  distance_UpDataTIme = time_UpDataTime = temp_UpDataTime = millis();
   change_time = millis();
+  alarm_delay = millis();
+
 }
 
 float test = 0;
+bool button_check_1 = false;
+bool button_check_2 = false;
+
 void loop() {
 
-  bluetooth_getData();
+  if (myRTC.hours == alarm_h && myRTC.minutes == alarm_m) {
+    tone(SP, 800);
+  }
+
+if(digitalRead(Button_Up) == true){
+  noTone(SP);
+}
+  //Button_alarmsetting
+  if (digitalRead(Button_setting) == false) {
+    button_check_1 = true;
+  }
+
+  if (digitalRead(Button_alarmsetting) == false) {
+    button_check_2 = true;
+  }
+
+  if (digitalRead(Button_setting) == true && button_check_1 == true) {
+    if (millis() - buttton_CheckTime >= 1000) {
+      tone(SP, 800);
+      delay(80);
+      noTone(SP);
+      button_check_1 = false;
+      SettingMode();
+    }
+  } else {
+    buttton_CheckTime = millis();
+  }
+
+
+
 
   if (millis() - change_time >= change_delay) {
     change_time = millis();
@@ -95,38 +138,97 @@ void loop() {
       average_count = 0;
       temperatureC = 0;
     }
-
     TemperatureDisplay(test);//온도 출력
 
   }
 
 }
 
-void bluetooth_getData() {
+bool check_sw = false;
+void SettingMode() {
+  myRTC.updateTime();//시갑 업데이트
+  int time_h = myRTC.hours, time_m = myRTC.minutes;
+  long delay_time = millis();
 
-  char c = bluetooth.read();
-  if (bluetooth.available()) {
-    digitalWrite(13, !digitalRead(13));
-    BT_getData += c;
+  while (true) {
+    TimeDisplay(time_h, time_m, true);
 
-  } else {
+    if (digitalRead(Button_setting) == false) {
+      check_sw = true;
+    }
+    if (digitalRead(Button_setting) == true && check_sw == true) {
+      check_sw = false;
+      myRTC.setDS1302Time(00, time_m, time_h, 6, 6, 7, 2023);
+      tone(SP, 800);
+      delay(80);
+      noTone(SP);
+      break;
 
-    if (BT_getData != "") {
-      Serial.println(BT_getData);
-      if (BT_getData.substring(0, 1) == "t") {
-        myRTC.setDS1302Time(
-          BT_getData.substring(1, 3).toInt(),//초
-          BT_getData.substring(3, 5).toInt(),//분
-          BT_getData.substring(5, 7).toInt(),//시
-          BT_getData.substring(7, 8).toInt(),//요일
-          BT_getData.substring(8, 10).toInt(),//일
-          BT_getData.substring(10, 12).toInt(),//월
-          BT_getData.substring(12, 16).toInt());//년
-      }
     }
 
-    BT_getData = "";
+    if (digitalRead(Button_Down) == true) {
+      if (millis() - delay_time >= 200) {
+        delay_time = millis();
+        time_m++;
+        if (time_m >= 60)time_m = 0;
+
+        tone(SP, 500);
+        delay(50);
+        noTone(SP);
+      }
+
+    }
+
+    if (digitalRead(Button_Up) == true) {
+      digitalWrite(13, true);
+      if (millis() - delay_time >= 200) {
+        delay_time = millis();
+        time_h++;
+        if (time_h >= 24)time_h = 0;
+
+        tone(SP, 500);
+        delay(50);
+        noTone(SP);
+      }
+
+    }
   }
+}
+
+
+void alarm_SettingMode() {
+  long delay_time = millis();
+  while (true) {
+    TimeDisplay(alarm_h, alarm_m, true);
+
+    if (digitalRead(Button_Down) == true) {
+      if (millis() - delay_time >= 200) {
+        delay_time = millis();
+        alarm_h++;
+        if (alarm_h >= 60)alarm_h = 0;
+
+        tone(SP, 500);
+        delay(50);
+        noTone(SP);
+      }
+
+    }
+
+    if (digitalRead(Button_Up) == true) {
+      digitalWrite(13, true);
+      if (millis() - delay_time >= 200) {
+        delay_time = millis();
+        alarm_m++;
+        if (alarm_m >= 24)alarm_m = 0;
+
+        tone(SP, 500);
+        delay(50);
+        noTone(SP);
+      }
+
+    }
+  }
+
 
 }
 
